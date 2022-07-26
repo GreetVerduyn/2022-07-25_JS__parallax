76807fc8019db42565ed1f3b611d74c2ab3deaaf
Greet Verduyn <greetverduyn@proximus.be>
2022-07-26 10:23:05 +0200

    basic functions and classes prepared, keydown events in consollllll

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function parallax(layer,distance, speed){
     const.item= document.querySelector(layer)
     item.transform ='translateX('+ -distance * speed + 'px)'
@@ -5,4 +6,55 @@ function parallax(layer,distance, speed){
 }
 document.addEventListener('scroll', function(){
     parallax('.floor, parallax.scrollX, 0.5')
-})
+    parallax('.Back,parallax.scroll, -0.5')
+
+    )
+})*/
+
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+    //event listeners, listens to keys pressed down
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', function(e){
+                console.log(e);
+
+            });
+        }
+
+
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    // adding,animation and removing enemies
+    function handleEnemies() {
+
+    }
+
+//display scores or messages
+    function displayStatusText() {
+
+    }
+
+    const input= new InputHandler();
+
+    // update and drawing the game
+    function animate() {
+
+    }
+
+});
